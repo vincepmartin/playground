2c724d2aa2a6c603096c54d7eef2337f580139ad
= <vince@finalatomicbuster.net>
2017-11-21 21:05:10 -0800

    leet code 101 done.

--- leetcode/101_symetric_tree_easy/answer.js
+++ leetcode/101_symetric_tree_easy/answer.js
@@ -4,44 +4,43 @@
      this.left = this.right = null;
  }
 
- /**
- * @param {TreeNode} root
- * @return {boolean}
+/**
+ * Returns true if the tree is a symetric.
+ * @param {TreeNode} root 
+ * @return {boolean} 
  */
-var isSymmetric = function(root) {
-    // Is symetric when.
-    // 1. left node of left tree is == right node of right tree.
-    // 2. right node of left tree is == left node of right tree.
-
-    let leftTreeNode = root.left;
-    let rightTreeNode = root.right;
-    
-    // Test for symetry in a recursive manner.
-    console.log(`Checking leftNode: ${leftTreeNode.left}, ${leftTreeNode.right} and rightNode: ${rightTreeNode.left}, ${rightTreeNode.right}`) 
-    if((leftTreeNode.left === rightTreeNode.right) && (leftTreeNode.right === rightTreeNode.left)) {
-        isSymetric(leftTreeNode);
-        isSymetric(rightTreeNode); 
+let isSymmetric = function(root) {
+    // Apparently a table that is null is symetric because it has no values? I had to change this for this to pass the leetcode test. 
+    if(root === null)
         return true;
-    }
 
-    else
-        return false;
+    // Hey hey, otherwise, let's go ahead and dive into our branch...
+    return nodesAreMirrored(root.left, root.right);
 };
 
 /**
- * Print out the binary tree in order. 
- * TODO: Fix bug that is causing it to attempt to recurse into a null object.
- * @param {TreeNode} rootNode 
+ * Return true if we have two nodes that are mirror images of each other.
+ * @param {TreeNode} node1 
+ * @param {TreeNode} node2
+ * @return {boolean} both nodes are mirrors.
  */
-let printTree = function(rootNode) {
-    console.log(rootNode); 
-    if(rootNode !== null) {
-        printTree(rootNode.left); 
-        console.log(`${rootNode.val} `);
-        printTree(rootNode.right);
+let nodesAreMirrored = function(node1, node2) {
+    // Base case 
+    // Both nodes are null ergo they are mirrored.
+    if(node1 === null && node2 === null) {
+        return true;
     }
-}
 
+    // Test recursively.
+    if( (node1 !== null) &&
+        (node2 !== null) &&
+        (node1.val === node2.val) ) {
+            return( nodesAreMirrored(node1.left, node2.right) && nodesAreMirrored(node1.right, node2.left) )
+        }
+
+    // If we've made it this far we are not a mirror.
+    return false;
+}
 
 /**
  * Create a binary tree w/ data given in an array. 
@@ -51,6 +50,8 @@ let printTree = function(rootNode) {
  */
 let generateTree = function(treeArray, rootNode, i) {
     if(i < treeArray.length) {
+        console.log(`Creating New tree w/ Current value of i: ${i} of array length ${treeArray.length}`);
+        // debugger;
         rootNode = new TreeNode(treeArray[i]); 
         rootNode.left = generateTree(treeArray, rootNode.left, 2*i + 1);
         rootNode.right = generateTree(treeArray, rootNode.right, 2*i+2);
@@ -59,8 +60,17 @@ let generateTree = function(treeArray, rootNode, i) {
     return rootNode;
 }
 
-tree = new TreeNode();
-values = [1, 2, 3, 4, 5, 6, 7];
-tree = generateTree(values, tree, 0);
-// console.log(tree);
-printTree(tree);
+// Test driver code.
+/* 
+let non_symetric_tree = new TreeNode();
+non_symetric_tree = generateTree([1, 2, 3, 4, 5, 6, 7], non_symetric_tree, 0);
+
+let symetric_tree = new TreeNode();
+symetric_tree = generateTree([1,2,2,3,4,4,3], symetric_tree, 0);
+*/
+
+let non_symetric_tree_2 = new TreeNode();
+non_symetric_tree_2 = generateTree([1,2,2,null,3,null,3], non_symetric_tree_2, 0);
+
+console.log(non_symetric_tree_2);
+console.log(`\n\n\tThe above tree is symetric: ${isSymmetric(non_symetric_tree_2)}.`);
