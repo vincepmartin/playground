045ffcebe5b9a2119df74acfdf1aedd45281b1bd
Vincent Martin <vince@finalatomicbuster.net>
2017-11-29 01:05:43 -0800

    works kind of, but fails some tests... [1,null,3,2] (passes) [1,2] fails

--- leetcode/110_balanced_binary_tree_easy/answer.js
+++ leetcode/110_balanced_binary_tree_easy/answer.js
@@ -5,7 +5,7 @@
  * @param {*} i 
  */
 let generateTree = function(treeArray, rootNode, i) {
-  if(i < treeArray.length && treeArray[i] !== null) {
+  if(i < treeArray.length) {
         rootNode = new TreeNode(treeArray[i]); 
         rootNode.left = generateTree(treeArray, rootNode.left, 2*i+1);
         rootNode.right = generateTree(treeArray, rootNode.right, 2*i+2);
@@ -20,39 +20,46 @@ function TreeNode(val) {
     this.left = this.right = null;
 }
 
+/**
+ * Getsthe depth of both the left and right branches of a tree.
+ * @param {TreeNode} root 
+ */
 let getDepthRecursive = function(root) {
     // Base case.
-    if(root === null) {
-        console.log(`Base case...`);
-        return 0;
-    }
-    
-    // When we are on a leaf node, we know we are at the bottom.
-    if(root.left === null && root.right === null) {
-        console.log(`Hit left and right as null`); 
-        return 1;
-    }
+    if(root === null)
+        return {left: -1, right: -1}
 
-    // Go down the left side.
-    if(root.left !== null) {
-        console.log(`left not null`);
-        return getDepthRecursive(root.left) + 1;
-    }
+    // Calculate the left and right depth.
+    let depth = {
+        left: (getDepthRecursive(root.left) === null ? 0 : getDepthRecursive(root.left).left) + 1,
+        right: (getDepthRecursive(root.right) === null ? 0 : getDepthRecursive(root.right).right) + 1
+    };
 
-    // Go down the left side.
-    if(root.right !== null) {
-        console.log(`right not null`);
-        return getDepthRecursive(root.right) + 1;
-    }
-
-    // Return
-    console.log(`left is: ${getDepthRecursive(root.left)} right is: ${getDepthRecursive(root.right)}`)
+    return depth;
 }
 
+let isBalanced = function(tree) {
+    let depth = getDepthRecursive(tree);
+    console.log(`L: ${depth.left} R: ${depth.right}`);
+
+    if(Math.abs(depth.left - depth.right) >= 1) {
+        return false;
+    }
+
+    else {
+        return true;
+    }
+} 
 
 // Test driver code...
 // let root = new TreeNode();
 let root;
-root = generateTree([1, 2, 3], root, 0); 
+let rootfalse;
+
+root = generateTree([1, 2], root, 0); 
+rootfalse = generateTree([1,null,2,null,3], rootfalse, 0);
 console.log(root);
-console.log(getDepthRecursive(root));
+console.log(`Balanced: ${isBalanced(root)}`);
+console.log(`******`);
+console.log(rootfalse);
+console.log(`Balanced: ${isBalanced(rootfalse)}`);
