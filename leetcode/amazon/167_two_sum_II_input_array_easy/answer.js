60756c12b739e1c8d9a52a267a43716c14526213
Vincent Martin <vince@finalatomicbuster.net>
2018-04-16 19:43:16 -0700

    finished 167 two sum

---
+++ leetcode/amazon/167_two_sum_II_input_array_easy/answer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  let answers = [] 
+  for(let i = 0; i < numbers.length; i++) {
+    let x = numbers[i]
+    
+    Inner:
+    for(let j = i + 1; j < numbers.length; j++) {
+      let y = numbers[j]
+      if(x + y === target){
+        answers.push(i + 1)
+        answers.push(j + 1)
+        break Inner
+      }
+    }
+  }
+  return answers
+};
+
+// Test.
+console.log(twoSum([2, 7, 11, 15], 9))
+
+console.log(twoSum([2, 3, 4], 6))
