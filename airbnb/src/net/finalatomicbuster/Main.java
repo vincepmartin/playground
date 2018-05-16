ff0f8aa4bca2f3c0e24b1567d631cbad8cd96f9e
Vincent Martin <vince@finalatomicbuster.net>
2018-05-15 17:54:53 -0700

    moving some stuff around.

--- airbnb/src/net/finalatomicbuster/Main.java
+++ airbnb/src/net/finalatomicbuster/Main.java
@@ -1,9 +1,15 @@
 package net.finalatomicbuster;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Java based leetcode problems!");
+        ArrayList<String> availableProblems = new ArrayList<>();
+
+        availableProblems.add("751");
+
         if(args.length < 1) {
             System.out.println("Submit a test value please.");
             return;
@@ -15,8 +21,14 @@ public class Main {
                 lc.run();
                 break;
 
+            case "list":
+                System.out.println("Available options: ");
+                availableProblems.forEach( o -> {
+                    System.out.println(o);
+                });
+
             default:
-                System.out.println("Nothing found.");
+                System.out.println("supply a # or use \"list\" to get a list of all possible values.");
         }
     }
 }
--- leetcode/amazon/readme.md
+++
@@ -1,2 +0,0 @@
-This is the top amazon questions from leetcode.
-https://leetcode.com/problemset/top-amazon-questions/
