0f38a96bbbea21495a1d8e391869afdcfb900fc9
Vincent Martin <vince@finalatomicbuster.net>
2018-09-13 21:51:58 -0700

    works.

--- interviews/buildingconnected/logs/solution/sync-sorted-merge.js
+++ interviews/buildingconnected/logs/solution/sync-sorted-merge.js
@@ -20,7 +20,6 @@ module.exports = (logSources, printer) => {
 
 	// We have an initial sorted array, now lets keep processing.
 	while(activeLogSources > 0) {
-		console.log(`Active Log Sources: ${activeLogSources}`)	
 		var loggersToPoll = []
 
 		// Print out the oldest logs found in the tree.
@@ -28,23 +27,20 @@ module.exports = (logSources, printer) => {
 
 		oldestLogs.forEach( (logContainer, index) => {
 			printer.print(logContainer.log)
-			console.log(`*** Pushing ${logContainer.sourceIndex}`)	
-			console.log(`Index: ${index}`)
 			loggersToPoll.push(logContainer.sourceIndex)
 		})
 
 		// Delete the logs that were printed.
-		oldestLogs.forEach( logContainer => { workingTree.delete(logContainer.sourceIndex) })	
+		oldestLogs.forEach( logContainer => {
+			workingTree.delete(logContainer.log.date.getTime()) 
+		})	
 
 		// Pop new logs off of the sources that were just used into our BST and then update the activeLogSources count as needed.
 		loggersToPoll.forEach( logSourceIndex => {
-			console.log(`***** Polling: ${logSourceIndex}`)	
 			let log = logSources[logSourceIndex].pop()
 			if(log === false) {
 				activeLogSources--
-				console.log(`***** DECREMENT LOG SOURCE COUNT ${activeLogSources}`)	
 			} else {
-				console.log(`***** Log Source Not NULL`)
 				workingTree.insert(log.date.getTime(), {sourceIndex: logSourceIndex, log: log})
 			}
 		})
