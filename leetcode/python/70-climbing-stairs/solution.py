4e7fe3ff870ec747268053092a5c23c5983d6af2
Vincent Martin <vmartin@fednat.com>
2019-06-13 21:19:23 -0700

    Updated/Fixed 70.

--- leetcode/python/70-climbing-stairs/solution.py
+++ leetcode/python/70-climbing-stairs/solution.py
@@ -1,21 +1,19 @@
 class Solution:
     def __init__(self):
-        self.totalPaths = 0 
-        # Some dynamic programming...
         self.cache = {}
 
     def climbStairs(self, n: int) -> int:
         if n == 2:
             return 2
-        
+
         elif n == 1:
             return 1
 
         if n not in self.cache:
             self.cache[n] = self.climbStairs(n-2) + self.climbStairs(n-1)
-        
+
         return self.cache[n]
 
-
 s = Solution()
-print(s.climbStairs(37))
+# Should be 21.
+print(s.climbStairs(7))
